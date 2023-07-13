9e08b20e80e6438ddd625ee071156aa3b0d65aab
Angel Serrato <defyv3@gmail.com>
2023-07-13 12:46:15 -0500

    Update advisorFilters.js
    clear filter selection

--- src/advisorFilters.js
+++ src/advisorFilters.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 function AdvisorFilter({ advisors, onFilter }) {
   const [selectedCerts, setSelectedCerts] = useState([]);
   const [certifications, setCertifications] = useState([]);
+  const [isFilterCleared, setIsFilterCleared] = useState(false);
 
   const handleCertChange = (cert) => {
     if (selectedCerts.includes(cert)) {
@@ -12,29 +13,31 @@ function AdvisorFilter({ advisors, onFilter }) {
     }
   };
 
+  const handleClearFilters = () => {
+    setSelectedCerts([]);
+    setIsFilterCleared(true);
+  };
+
   useEffect(() => {
     const uniqueCerts = [...new Set(advisors.flatMap((advisor) => advisor.certs.filter(cert => cert !== "")))];
     setCertifications(uniqueCerts);
-    onFilter(selectedCerts); // Notify parent component of selected certifications
-  }, [advisors, selectedCerts, onFilter]);
+    onFilter(selectedCerts);
+  }, [advisors, selectedCerts, onFilter, isFilterCleared]);
 
   return (
     <div className="filterTitle">
-     
-      
       <div className="certPills">
-      {/* <label htmlFor="certSelect">
-        <p className=" certFilterTitle"> Filter by Certification </p>
-     </label> */}
         {certifications.map((cert) => (
           <div
             key={cert}
             className={`certPill ${selectedCerts.includes(cert) ? 'selected' : ''}`}
             onClick={() => handleCertChange(cert)}
           >
-            {cert}
+            {cert} 
+            
           </div>
         ))}
+        <button onClick={handleClearFilters}>Clear Filters</button>
       </div>
     </div>
   );
