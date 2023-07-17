b56a122d6955b1883c80c985d2b66afd6fd59d32
Chaeil (Robert) Yun <robertyun625@gmail.com>
2023-07-16 21:42:19 -0500

    Has additional Details page

--- src/moreDetails.js
+++ src/moreDetails.js
@@ -9,17 +9,42 @@ function MoreDetails({ advisors }) {
         onMouseEnter={() => setShowDetails(true)}
         onMouseLeave={() => setShowDetails(false)}
       >
+        <div className="more-details-loc">More Details</div>
+  
         {showDetails && (
           <div className="details-content">
-            {/* Display the details content here */}
-            <h2>{advisors.advisorName}</h2>
-            {/* Add other details properties as needed */}
+            <div className="details-row">
+              <div className="details-item">
+                <h3>Location:</h3>
+                <p>{Array.isArray(advisors.geography) && advisors.geography.length > 0 && advisors.geography[0] !== "" && ` ${advisors.geography.join(", ")}`}</p>
+              </div>
+              <div className="details-item">
+                <h3>Services:</h3>
+                <p>{Array.isArray(advisors.serviceLocation) && advisors.serviceLocation.length > 0 && advisors.serviceLocation[0] !== "" && ` ${advisors.serviceLocation.join(", ")}`}</p>
+              </div>
+            </div>
+            <div className="details-row">
+              <div className="details-item">
+                <h3>Wealth Continua:</h3>
+                <p>{Array.isArray(advisors.clientSize) && advisors.clientSize.length > 0 && advisors.clientSize[0] !== "" && ` ${advisors.clientSize.join(", ")}`}</p>
+              </div>
+              <div className="details-item">
+                <h3>Client Niche:</h3>
+                <p>{Array.isArray(advisors.clientType) && advisors.clientType.length > 0 && advisors.clientType[0] !== "" && ` ${advisors.clientType.join(", ")}`}</p>
+              </div>
+            </div>
+            <div className="details-row">
+              <div className="details-item">
+                <h3>Field of Expertise:</h3>
+                <p>{Array.isArray(advisors.technica) && advisors.technica.length > 0 && advisors.technica[0] !== "" && ` ${advisors.technica.join(", ")}`}</p>
+              </div>
+            </div>
           </div>
         )}
-        More Details...
       </div>
     );
   }
+  
   
 
 
